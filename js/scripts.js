69e9b5e15c1f75a655d4ddd80fa56ed7307582a3
Nexovus <92540383+Nexovus@users.noreply.github.com>
2021-10-14 23:23:44 -0400

    Added event section filters template
    Includes event-tile.html template for use with rigged ajax call

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+var data = {
+    archived: 0,
+    name: '',
+    location: '',
+    lengthMin: 0,
+    lengthMax: 8,
+    costMin: 0,
+    costMax: 500,
+    sizeMin: 0,
+    sizeMax: 1000
+}
+
+function getEvents(params) {
+    $.ajax({
+        type: 'GET',
+        url: '#',
+        data: data
+    })
+}
+
+function generateEvents(params) {
+
+}
+
+function updateEvents(params) {
+    
+}
+
 // 'DocReady / self executing function'
 $(function () {
     $('.site-nav a').each(function () {
@@ -5,4 +33,5 @@ $(function () {
             $(this).addClass('active-link');
         }
     })
+    getEvents();
 });
